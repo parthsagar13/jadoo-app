190dc604ed1eeb72917e641f8a58e7c033dc15d8
Parth Sagar <kallathiyaparth744@gmail.com>
2023-02-02 00:50:28 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './Componets/Navbar';
-
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route exact path='/' element={<Navbar/>}/>
-    </Routes>
-  </BrowserRouter>
+    <div>
+   
+  </div>
   );
 }
 
